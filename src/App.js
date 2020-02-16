364e0a441ce496661eb263866ce3625b1e332cb4
Enes Tasbasi <enestasbasi@outlook.com>
2020-02-15 23:44:20 -0500

    Initial commit, start the landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRibbon } from '@fortawesome/free-solid-svg-icons'
+import { Button } from 'react-materialize';
+
+import './App.scss';
+
+
+function Landing() {
+  return (
+    <div className="landing">
+      <span className="ribbon-icon">
+        <FontAwesomeIcon icon={faRibbon} />
+      </span>
+      <h1>Epilepsy Daily</h1>
+
+      <div className="buttons">
+        <Button
+          node="button"
+          className="login"
+          waves="light"
+        >
+          LOGIN
+        </Button>
+        <Button
+          flat
+          node="button"
+          className="login"
+          waves="light"
+        >
+          SIGNUP
+        </Button>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+        </Switch>
+      </Router>
     </div>
   );
 }
